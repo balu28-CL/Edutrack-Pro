fc8218b65f17374a2971a14ce7637184ad74c548
Balaji <yenjalabalu28@gmail.com>
2026-08-18 18:36:13 +0530

    Fix login API for production

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
         try {
 
             const response = await axios.post(
-                "http://localhost:5000/api/auth/login",
+                "https://edutrack-pro-backend.onrender.com/api/auth/login",
                 {
                     email,
                     password
